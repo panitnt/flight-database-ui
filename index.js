6f2fac4bdf452a9fa20699b90e716faf8fef415a
natekrth <natekrth@gmail.com>
2023-04-06 00:52:12 +0700

    boostrap and nav bar

--- index.js
+++ index.js
@@ -63,7 +63,6 @@ const seacrhFlightEmployee = async (res, flightnum) => {
   try {
     let con = await sql.connect(string_connection);
     let request = new sql.Request(con);
-    // console.log(flightnum)
     const result = await request.query(`SELECT * FROM FlightEmployee fe, Employee e, FlightPlan fp, Airplane a, Airline air WHERE fe.flightPlanID = fp.PlanID and fe.emp_ID = e.emp_ID and fp.planeID = a.planeID and air.airlineID = e.airlineID and fp.FlightNumber = '${flightnum}'`);
     return result;
   } catch (err) {
@@ -86,36 +85,42 @@ app.use(bodyParser.urlencoded( { extended : false }));
 app.get("/", function (req, res) {
   res.render("home");
 });
+
 app.get("/flight", async function (req, res) {
   let result = await listAllFlight(res);
   res.render("flight", {
     flightResult: result.recordset,
   });
 });
+
 app.get("/passenger", async function (req, res) {
   let result = await passenger(res);
   res.render("passenger", {
     passengerResult: result.recordset,
   });
 });
+
 app.get("/reserve", async function (req, res) {
   let result = await reserve(res);
   res.render("reserve", {
     reserveResult: result.recordset,
   });
 });
+
 app.get("/flight-employee", async function (req, res) {
   let result = await flightEmployee(res);
   res.render("flightEmployee", {
     flightEmployeeResult: result.recordset,
   });
 });
+
 app.post("/search-flight-employee", async function (req, res) {
   let result = await seacrhFlightEmployee(res, req.body.flightn);
   res.render("flightEmployee", {
     flightEmployeeResult: result.recordset,
   });
 });
+
 app.listen(8083, "localhost", () => {
   console.log("URL: http://localhost:8083");
 });
